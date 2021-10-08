32d07fecb7cd9dc6af18619390d2c4a571a83772
Charlyzzz <erwincdl@gmail.com>
2021-10-08 13:54:25 -0300

    Fix report period

--- go.mod
+++ go.mod
@@ -3,10 +3,20 @@ module github.com/10Pines/tracker/v2
 go 1.16
 
 require (
-	github.com/10Pines/tracker v0.0.0-20210927181928-1d2d3dc2e432
 	github.com/gin-gonic/gin v1.7.4
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/slack-go/slack v0.9.5
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63 // indirect
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.1.5
 	gorm.io/gorm v1.21.15
 )
--- internal/schedule/report.go
+++ internal/schedule/report.go
@@ -12,29 +12,30 @@ import (
 
 // PeriodicallyRunReport runs a report at 12PM, every 24 hours
 func PeriodicallyRunReport(db *gorm.DB, reporter reporter.Reporter) {
-	wait := timeUntilNextReport(time.Now())
-	time.Sleep(wait)
-	ticker := time.Tick(24 * time.Hour)
 	for {
-		select {
-		case <-ticker:
-			r, err := report.Run(db)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			err = reporter.Process(r)
-			if err != nil {
-				log.Println(err)
-			}
+		wait := timeUntilNextReport(time.Now())
+		logWaitTime(wait)
+		time.Sleep(wait)
+		r, err := report.Run(db)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = reporter.Process(r)
+		if err != nil {
+			log.Println(err)
 		}
 	}
 }
 
+func logWaitTime(wait time.Duration) {
+	log.Printf("the next report is due in %.2f hs", wait.Round(time.Hour).Hours())
+}
+
 func timeUntilNextReport(now time.Time) time.Duration {
-	firstRun := time.Date(now.Year(), now.Month(), now.Day(), 24, 0, 0, 0, time.UTC)
-	if firstRun.Before(now) {
-		firstRun = firstRun.AddDate(0, 0, 1)
+	nextRun := time.Date(now.Year(), now.Month(), now.Day(), 24, 0, 0, 0, time.UTC)
+	if nextRun.Before(now) {
+		nextRun = nextRun.AddDate(0, 0, 1)
 	}
-	return firstRun.Sub(now)
+	return nextRun.Sub(now)
 }
